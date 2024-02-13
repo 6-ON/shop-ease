cda9fa396edd33cc4a4f60dc038f18753d1231fa
6-ON <aminhatim6@gmail.com>
2024-02-13 14:07:42 +0100

    SE-25 add tests

--- App.tsx
+++ App.tsx
@@ -3,9 +3,9 @@ import { store } from './src/store'
 import React from 'react'
 import 'react-native-gesture-handler';
 import { App } from './src'
-const MainApp = () => (
+const Entry = () => (
 	<Provider store={store}>
 			<App />
 	</Provider>
 )
-export default MainApp
+export default Entry
